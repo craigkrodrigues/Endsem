9b7c52d977f62dd627fa796d2c3e5e3ece484ec2
$_$ <$_$@DESKTOP-SRGRKRM>
2017-06-08 14:35:24 +0530

    First class

---
+++ Endsem/src/Candidate.java
@@ -0,0 +1,110 @@
+public class Candidate
+
+	{
+
+	 
+
+	    private String name;   
+
+	    private int votes;
+
+	 
+
+	     
+
+	    /**
+
+	     * Candidate Constructor
+
+	     *
+
+	     * @param name of candidate
+
+	     */
+
+	    public Candidate(String name)
+
+	    {       
+
+	        this.name = name;
+
+	        votes = 0;
+
+	    }
+
+	 
+
+	    /**
+
+	     * assigns a name to a candidate
+
+	     *
+
+	     * @param name of candidate
+
+	     */
+	    public void setName(String name)
+
+	    {
+
+	        this.name = name;
+
+	    }
+
+	     
+
+	    /**
+
+	     * adds 1 to the number of votes cast
+
+	     *
+
+	     */
+
+	    public void incrementVoteCount()
+
+	    {
+
+	        votes++;
+
+	    }
+
+	 
+	    /**
+
+	     * provide the candidate's name
+
+	     *
+
+	     * @return candidate's name
+
+	     */
+
+	    public String getName()
+	    {
+
+	        return name;
+
+	    }
+
+	     
+	    /**
+
+	     * returns the number of votes for the candidate
+
+	     *
+
+	     * @return number of votes
+	     */
+
+	    public int getNumberOfVotes()
+
+	    {
+
+	        return votes;
+
+	    }
+
+	 
+
+	}
